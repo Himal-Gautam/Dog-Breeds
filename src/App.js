ece761721e21a58573b434e448e6680f059c191d
Himal Gautam <himalpgautam@gmail.com>
2023-05-07 23:45:20 +0530

    error resolution on deployment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { react, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
